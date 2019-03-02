bdd33bac21e94c0f5313f22a35e971546aafebb0
ChandlerBaskins <43590431+piminto@users.noreply.github.com>
2019-03-02 06:59:48 -0600

    first commit

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+{ 
+    id: 0,
+    name: "chandlerbasdkins",
+    username: "youngchee",
+    email: "im not sjure",
+},
+{ 
+    id: 2,
+    name: "chandler",
+    username: "youndddaadasgchee",
+    email: "im asdqaswdnot sjure",
+},
+{ 
+    id: 3,
+    name: "chandlddder",
+    username: "youngcheasdasdsade",
+    email: "im not sjure",
+},
+]
